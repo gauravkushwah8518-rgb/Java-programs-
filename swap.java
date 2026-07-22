091b6c64df4ede0e920d5c0866dc528997f19e60
Your GitHub Username <your-email@example.com>
2026-07-22 11:15:00 +0530

    new program

---
+++ swap.java
@@ -0,0 +1,11 @@
+public class swap {
+public static void main (String [] args ){
+    int m = 5;, n = 10;
+    System.out.println("before swapping "+ m +",n = " + n );
+     int temp = m;
+     m = n ;
+     n = temp;
+     System.out.println("After swapping: m = " + m + ", n = " + n);
+}
+    
+}
