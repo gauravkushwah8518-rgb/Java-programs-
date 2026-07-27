16cf4cd9f91f07a35ca042a96a097d87b2130d1f
Your GitHub Username <your-email@example.com>
2026-07-27 14:43:14 +0530

    newsprogram

---
+++ quadrant.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class quadrant
+{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int x = 0;
+        int y = 0;
+        System.err.print("Enter the x-coordinate: ");5
+        x = scanner.nextInt();
+        System.err.print("Enter the y-coordinates: ");
+        y = scanner.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println("Quadrant 1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Quadrant 2");
+
+        } else if (x < 0 && y < 0) {
+            System.out.println("Quadrant 3");
+
+        } else if (x > 0 && y < 0) {
+            System.out.println("Quadrant 4");
+        }
+
+        else if (x == 0 || y == 0) {
+            System.out.println("On Axis");
+        }
+    }
+}
